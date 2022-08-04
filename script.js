59f0e84b757194552a8b6b3c185f6996ec6a4da5
pao <aungpao321@gmail.com>
2022-08-04 14:10:50 +0700

    1

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+const imgP = document.getElementById("img-profile")
+const nameP = document.getElementById("p-name")
+const mailP = document.getElementById("p-email")
+const addP = document.getElementById("p-address")
+const btnR = document.getElementById("btn-random")
+const genderP = document.getElementById("span-gender-icon")
+const divP = document.getElementById("div-user-card")
+const divL = document.getElementById("div-loading-card")
+callApi()
+
+imgP.onload = () => {
+  divL.style.display = ""
+  divP.style.display = ""
+}
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  imgP.src = resp.data.results[0].picture.large
+  nameP.innerText = `${resp.data.results[0].name.first} ${resp.data.results[0].name.last}`
+  mailP.innerText = resp.data.results[0].email
+  addP.innerText = `${resp.data.results[0].location.city} ${resp.data.results[0].location.state} ${resp.data.results[0].location.country} ${resp.data.results[0].location.postcode}`
+  if(resp.data.results[0].gender == "male"){
+    genderP.innerText = "👨"
+  }else{
+    genderP.innerText = "👩"
+  }
+  divL.style.display = "none"
+  divP.style.display = ""
+}
+
+btnR.onclick = () => {
+  callApi()
+  divL.style.display = ""
 }
